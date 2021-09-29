95c3e748a46d54dba59411e5259c857ae7128cc7
elifelse <elifgsavukcam@gmail.com>
2021-09-29 12:00:39 +0300

    Product filters / brand-category

--- src/Web/Services/HomeViewModelService.cs
+++ src/Web/Services/HomeViewModelService.cs
@@ -1,5 +1,8 @@
 ﻿using ApplicationCore.Entities;
 using ApplicationCore.Interfaces;
+using ApplicationCore.Specifications;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Web.Interfaces;
@@ -10,13 +13,18 @@ namespace Web.Services
     public class HomeViewModelService : IHomeViewModelService
     {
         private readonly IAsyncRepository<Product> _productRepository;
-        public HomeViewModelService(IAsyncRepository<Product> productRepository)
+        private readonly IAsyncRepository<Category> _categoryRepository;
+        private readonly IAsyncRepository<Brand> _brandRepository;
+        public HomeViewModelService(IAsyncRepository<Product> productRepository, IAsyncRepository<Category> categoryRepository, IAsyncRepository<Brand> brandRepository)
         {
             _productRepository = productRepository;
+            _categoryRepository = categoryRepository;
+            _brandRepository = brandRepository;
         }
-        public async Task<HomeViewModel> GetHomeViewModelAsync()
+        public async Task<HomeViewModel> GetHomeViewModelAsync(int? categoryId, int? brandId)
         {
-            var products = await _productRepository.ListAllAsync();
+            var specProducts = new ProductsFilterSpecification(categoryId, brandId);
+            var products = await _productRepository.ListAsync(specProducts);
 
             var vm = new HomeViewModel()
             {
@@ -26,10 +34,26 @@ namespace Web.Services
                     ProductName = p.ProductName,
                     PictureUri = p.PictureUri,
                     Price = p.Price
-                }).ToList()
+                }).ToList(),
+                Categories = await GetCategoriesAsync(),
+                Brands = await GetBrandsAsync(),
+                CategoryId = categoryId,
+                BrandId = brandId
             };
 
             return vm;
+        }
+
+        private async Task<IEnumerable<SelectListItem>> GetBrandsAsync()
+        {         
+            var brands = await _brandRepository.ListAllAsync();
+            return brands.Select(b => new SelectListItem(b.BrandName, b.Id.ToString()));
+        }
+
+        private async Task<IEnumerable<SelectListItem>> GetCategoriesAsync()
+        {
+            var categories = await _categoryRepository.ListAllAsync();
+            return categories.Select(c => new SelectListItem(c.CategoryName, c.Id.ToString()));
         }
     }
 }
