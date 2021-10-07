54b614befc0894c6ef949bddfeb364eb1d41cc9b
elifelse <elifgsavukcam@gmail.com>
2021-10-07 14:31:14 +0300

    Order and Order Checkout  / PictureUri type fixed in database.

--- src/ApplicationCore/Entities/OrderDetail.cs
+++ src/ApplicationCore/Entities/OrderDetail.cs
@@ -6,7 +6,7 @@
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public decimal Price { get; set; }
-        public decimal PictureUri { get; set; }
+        public string PictureUri { get; set; }
         public int Quantity { get; set; }
 
         public Product Product { get; set; }
---
+++ src/ApplicationCore/Interfaces/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using ApplicationCore.Entities;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IOrderService
+    {
+        Task<int> CreateOrderAsync(int basketId, Address address);
+    }
+}
